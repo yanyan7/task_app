54779caca7dc53eb6667bd5b43c3ded3528cc960
yanyan7 <sakuraperfection@gmail.com>
2021-01-10 17:08:35 +0900

    基本機能の実装完了

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "登録しました"
+      redirect_to @task
+      # redirect_to task_url(@task)    #  上のはこれでも良い
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = "更新しました"
+      redirect_to @task
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
+end
