5e590d60381949d00d25399f3083977debdc75e9
Ubuntu <ubuntu@ip-172-31-1-107.ap-northeast-1.compute.internal>
2021-01-12 09:04:25 +0000

    userのcrud完了。valideteとログイン機能をつける。

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_085132) do
+ActiveRecord::Schema.define(version: 2021_01_12_071228) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
