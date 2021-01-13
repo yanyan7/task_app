24336410b2c294ec7e3e47110910b0f8964e53f9
yanyan7 <sakuraperfection@gmail.com>
2021-01-13 11:08:36 +0900

    UserのCRUD完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_071228) do
+ActiveRecord::Schema.define(version: 2021_01_13_002247) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_071228) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
